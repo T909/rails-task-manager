e9af6d9c4b54fa70d42d34b7955a36be891bd30b
Ijoline Homphomsiltham <ijoline@gmail.com>
2019-08-20 16:48:16 +0100

    Crud with refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,11 @@
 class TasksController < ApplicationController
+  before_action :fetch_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
   def new
     @task = Task.new
@@ -16,15 +18,15 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to @task
   end
   def detroy
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.delete
     redirect_to tasks_path
   end
@@ -34,4 +36,9 @@ private
   def task_params
     params.require(:task).permit(:id, :title, :details, :completed)
   end
+
+  def fetch_task
+    @task = Task.find(params[:id])
+  end
+
 end
